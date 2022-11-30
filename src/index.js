72a0a383aa0b8544fc6617d1518962856d6362ab
shadracknyaribo <stilespulsar77@gmail.com>
2022-11-30 10:49:44 -0800

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 // write your code here
+let newname = document.getElementById("new-name").value;
+let restaurant = document.getElementById("new-restaurant").value;
+let image = document.getElementById("new-image").value;
+fetch('http://localhost:3000',{
+    method: 'GET',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newname,restaurant,image)
+})
+.then(res =>{
+
+return res.json();})
+.then(data => console.log(data))
+
+
+function ramendetail(){
+    let ramenDetail = document.getElementById("ramen-detail");
+    ramenDetail.toggle('reveal');
+    let ramenMenu = document.getElementById("ramen-menu");
+    ramenMenu.addEventListener("click", ramendetail);
+
+}
