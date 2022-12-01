600a5bc569a1c04fd920823ad5188b482a38a2b5
shadracknyaribo <stilespulsar77@gmail.com>
2022-12-01 10:23:19 -0800

    done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 // write your code here
-let newname = document.getElementById("new-name").value;
+let newName = document.getElementById("new-name").value;
 let restaurant = document.getElementById("new-restaurant").value;
 let image = document.getElementById("new-image").value;
+//fetch syntax in js?
 fetch('http://localhost:3000',{
     method: 'GET',
     method: 'POST',
     headers: {
+        'Accept': 'application/json',
         'Content-Type': 'application/json'
     },
-    body: JSON.stringify(newname,restaurant,image)
+    body: JSON.stringify(newName,restaurant,image)
 })
 .then(res =>{
 
